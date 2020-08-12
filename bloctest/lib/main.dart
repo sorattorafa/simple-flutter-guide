5450cef4142fb38f9471aa79a2d5d3c04364b8ee
Rafael Soratto <rafael.soratto@appmoove.com.br>
2020-08-12 17:43:16 -0300

    bloc example

--- bloctest/lib/main.dart
+++ bloctest/lib/main.dart
@@ -1,63 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:bloctest/model.dart';
-import 'package:bloctest/repository.dart';
+import 'package:bloctest/viewmodel.dart';
 void main() => runApp(BlocApp());
 
 class BlocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter para Iniciantes - BLoC',
+      title: 'Aprendendo BLoC',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Repository bloc = Repository();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter para Iniciantes - BLoC'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder<int>(
-                stream: bloc.minhaStream,
-                initialData: 0,
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Há um erro na Stream');
-                  } else {
-                    return Text(
-                      '${snapshot.data}',
-                      style: Theme.of(context).textTheme.bodyText1,
-                    );
-                  }
-                }),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: bloc.incrementar,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-  @override
-  void dispose() {
-    bloc.fecharStream();
-    super.dispose();
   }
 }
---
+++ bloctest/lib/viewmodel.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:bloctest/repository.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Repository bloc = Repository();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aprendendo BLoC'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            StreamBuilder<int>(
+                stream: bloc.minhaStream,
+                initialData: 0,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Há um erro na Stream');
+                  } else {
+                    return Text(
+                      '${snapshot.data}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    );
+                  }
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: bloc.incrementar,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    bloc.fecharStream();
+    super.dispose();
+  }
+}
